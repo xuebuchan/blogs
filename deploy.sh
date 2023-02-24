c832f17be4220f3787b5e7878ff88802cd4863af
xuwenhui <1020884375@qq.com>
2023-02-24 16:34:55 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xuebuchan/blogs.git main:gh-pages
+git push -f git@github.com:xuebuchan/blogs.git master:gh-pages
 
 cd -
